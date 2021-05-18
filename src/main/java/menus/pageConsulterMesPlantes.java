e27d4a6a636633a6f911f4da5e58be3adaf5c4b5
Thomas Cournil <cournilthomas@gmail.com>
2021-05-18 15:21:36 +0200

    Modifications Consulter plantes

--- src/main/java/menus/pageConsulterMesPlantes.java
+++ src/main/java/menus/pageConsulterMesPlantes.java
@@ -5,6 +5,8 @@ import java.awt.Font;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -21,18 +23,19 @@ import plante.Plante;
 import util.Context;
 
 public class pageConsulterMesPlantes {
-	
+
 	public static JFrame jf;
-	public static JTextField textField;
-	public static JTextField textField2;
+	public static List<JTextField> textFields=new ArrayList();;
+	//public static JPanel panel;
 	public static int j=0;
-	
-	
+	public static int i=0;
+
+
 	/**
 	 * @wbp.parser.entryPoint
 	 */
 	public static void genererPageConsulterMesPlantes () {
-		
+
 		j=0;
 
 		jf = new JFrame("Thounsand Sunny's Little Garden");
@@ -40,8 +43,8 @@ public class pageConsulterMesPlantes {
 		jf.setBounds(400, 20, 450, 700);
 		jf.getContentPane().setLayout(null);
 
-		JButton btnNewButton = new JButton("Ajouter \u00E0 mes plantes");
-		
+				
+
 		JButton btnNewButton_1 = new JButton("Retour");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -51,10 +54,20 @@ public class pageConsulterMesPlantes {
 		});
 		btnNewButton_1.setBounds(0, 10, 85, 21);
 		jf.getContentPane().add(btnNewButton_1);
-		btnNewButton.setFont(new Font("Goudy Old Style", Font.BOLD, 30));
-		btnNewButton.setForeground(new Color(47, 79, 79));
-		btnNewButton.setBounds(20, 565, 400, 40);
-		jf.getContentPane().add(btnNewButton);
+
+		JButton btnNewButton_3 = new JButton("Supprimer");
+		btnNewButton_3.setForeground(new Color(47, 79, 79));
+		btnNewButton_3.setFont(new Font("Goudy Old Style", Font.BOLD, 25));
+		btnNewButton_3.setBounds(288, 565, 132, 40);
+		jf.getContentPane().add(btnNewButton_3);
+
+		JButton btnNewButton_2 = new JButton("Consulter");
+		btnNewButton_2.setForeground(new Color(47, 79, 79));
+		btnNewButton_2.setFont(new Font("Goudy Old Style", Font.BOLD, 25));
+		btnNewButton_2.setBounds(145, 565, 130, 40);
+		jf.getContentPane().add(btnNewButton_2);
+
+
 
 		JPanel panel = new JPanel();
 		panel.setOpaque(false);
@@ -62,8 +75,9 @@ public class pageConsulterMesPlantes {
 		jf.getContentPane().add(panel);
 		panel.setLayout(null);
 		
+		j=0;
 		for (MaPlante plante : App.mesPlantes) {
-			
+
 			JRadioButton rdbtnNewRadioButton_1 = new JRadioButton(plante.getSurnom());
 			rdbtnNewRadioButton_1.setForeground(new Color(255, 255, 255));
 			rdbtnNewRadioButton_1.setBackground(new Color(34, 139, 34));
@@ -80,11 +94,13 @@ public class pageConsulterMesPlantes {
 			lblNewLabel1.setBounds(110, 33+j, 50, 23);
 			panel.add(lblNewLabel1);
 
+			JTextField textField;	
 			textField = new JTextField();
 			textField.setColumns(10);
 			textField.setBounds(170, 35+j, 65, 19);
 			panel.add(textField);
-			
+			textFields.add(textField);
+
 			JLabel lblNewLabel2 = new JLabel("Date");
 			lblNewLabel2.setForeground(new Color(255, 255, 255));
 			lblNewLabel2.setFont(new Font("Goudy Old Style", Font.BOLD, 12));
@@ -94,15 +110,51 @@ public class pageConsulterMesPlantes {
 			lblNewLabel2.setBounds(245, 33+j, 50, 23);
 			panel.add(lblNewLabel2);
 
+			JTextField textField2;	
 			textField2 = new JTextField();
 			textField2.setColumns(10);
 			textField2.setBounds(305, 35+j, 65, 19);
 			panel.add(textField2);
-			
+			textFields.add(textField2);
+
 			j=j+28;
-			
+
 		}
 		
+		JButton btnNewButton = new JButton("Modifier");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				i=0;
+				for (MaPlante plante : App.mesPlantes) {
+
+					if(!textFields.get(0+i).getText().equals("")) {
+							App.mesPlantes.remove(plante);
+							MaPlante tmp=Context.get_instance().getDaoMP().findById(plante.getId());
+							Context.get_instance().getDaoMP().save(tmp);
+							App.mesPlantes.add(tmp);
+						
+					}
+					if(!textFields.get(1+i).getText().equals("")) {
+							App.mesPlantes.remove(plante);
+							MaPlante tmp=Context.get_instance().getDaoMP().findById(plante.getId());
+							Context.get_instance().getDaoMP().save(tmp);
+							App.mesPlantes.add(tmp);
+						
+					}
+					
+					i=i+2;
+
+				}
+				jf.setVisible(false);
+				pageMesPlantes.genererPageMesPlantes();
+
+			}
+		});
+		btnNewButton.setFont(new Font("Goudy Old Style", Font.BOLD, 25));
+		btnNewButton.setForeground(new Color(47, 79, 79));
+		btnNewButton.setBounds(20, 565, 115, 40);
+		jf.getContentPane().add(btnNewButton);
+
 
 		JLabel lblImages = new JLabel("");
 		lblImages.setHorizontalAlignment(SwingConstants.CENTER);
@@ -111,14 +163,14 @@ public class pageConsulterMesPlantes {
 		lblImages.setIcon(new ImageIcon(pageCreerCompte.class.getResource("/images/MesPlantes450x700.png")));
 		lblImages.setBounds(0, 0, 436, 664);
 		jf.getContentPane().add(lblImages);
-		
+
 		jf.setVisible(true);
 
-		
+
 	}
-	
+
 	public static void main (String[] args) {
-		
+
 	}
 
 }
